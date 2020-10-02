8ae9f464ad3e1e16d441151a2e2cb01c02828da4
Kishor Naik <kishor.naik011.net@outlook.com>
2020-10-02 10:36:24 +0530

    Code Commit

---
+++ Sol_Demo/Sol_Demo/Pages/Demo/ChildComponent.razor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sol_Demo.Pages.Demo
+{
+    public partial class ChildComponent
+    {
+        #region Private Property
+
+        private bool? IsDisplay { get; set; }
+
+        #endregion Private Property
+
+        #region Private Event Handler
+
+        public void OnCancelModel()
+        {
+            IsDisplay = false;
+            base.StateHasChanged();
+        }
+
+        #endregion Private Event Handler
+
+        #region Public Method
+
+        public void ShowModel()
+        {
+            IsDisplay = true;
+            base.StateHasChanged();
+        }
+
+        #endregion Public Method
+    }
+}
---
+++ Sol_Demo/Sol_Demo/Pages/Demo/ParentComponent.razor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sol_Demo.Pages.Demo
+{
+    public partial class ParentComponent
+    {
+        #region Private Method
+
+        private ChildComponent Child { get; set; }
+
+        #endregion Private Method
+
+        #region Event Handler
+
+        public void OnShowModelClick()
+        {
+            Child.ShowModel();
+        }
+
+        #endregion Event Handler
+    }
+}
