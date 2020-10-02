9759bdf7af003fe972dfac070b896be00bf8bb01
Kishor Naik <kishor.naik011.net@outlook.com>
2020-10-02 19:42:09 +0530

    Code Commit

---
+++ ExampleCards/Sol_Demo/Sol_Demo/Pages/Controls/CardComponent.razor.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sol_Demo.Pages.Controls
+{
+    public partial class CardComponent
+    {
+        #region Public Parameter Property
+
+        [Parameter]
+        public String BackgroundColor { get; set; }
+
+        [Parameter]
+        public String ForegroundColor { get; set; }
+
+        [Parameter]
+        public String Width { get; set; }
+
+        [Parameter]
+        public RenderFragment Content { get; set; }
+
+        #endregion Public Parameter Property
+
+        #region Private Property
+
+        private String Display { get; set; }
+
+        #endregion Private Property
+
+        #region Public Method
+
+        public void SetVisible(bool visible)
+        {
+            if (visible)
+            {
+                Display = "";
+            }
+            else
+            {
+                Display = "d-none";
+            }
+
+            base.StateHasChanged();
+        }
+
+        #endregion Public Method
+    }
+}
---
+++ ExampleCards/Sol_Demo/Sol_Demo/Pages/Demo/DemoComponent.razor.cs
@@ -0,0 +1,62 @@
+﻿using Sol_Demo.Pages.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sol_Demo.Pages.Demo
+{
+    public partial class DemoComponent
+    {
+        #region Cards Referance Property
+
+        private CardComponent Card1 { get; set; }
+        private CardComponent Card2 { get; set; }
+
+        #endregion Cards Referance Property
+
+        #region Private Property
+
+        private String HideToggleButtonText { get; set; }
+
+        #endregion Private Property
+
+        #region Public Event Handler
+
+        public void OnHideShowCardsEventHandler()
+        {
+            if (HideToggleButtonText == "Hide")
+            {
+                Card1.SetVisible(false);
+                Card2.SetVisible(false);
+
+                HideToggleButtonText = "Show";
+            }
+            else if (HideToggleButtonText == "Show")
+            {
+                Card1.SetVisible(true);
+                Card2.SetVisible(true);
+
+                HideToggleButtonText = "Hide";
+            }
+
+            base.StateHasChanged();
+        }
+
+        #endregion Public Event Handler
+
+        #region Protected Method
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            if (firstRender)
+            {
+                HideToggleButtonText = "Hide";
+
+                base.StateHasChanged();
+            }
+        }
+
+        #endregion Protected Method
+    }
+}
